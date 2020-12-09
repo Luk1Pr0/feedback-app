25be1c63d78192e78471b6dd632829d37503c1a1
Luk1Pr0 <lpytel16@gmail.com>
2020-12-09 17:52:55 +0100

    Styling for mobile devices applied

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const registerBtn = document.querySelectorAll(".nav-link")[1];
 const signinPage = document.getElementById("signin-page");
 const registerPage = document.getElementById("register-page");
 // Sign in and register forms
-const signinForm = document.querySelectorAll("form")[0];
-const registerForm = document.querySelectorAll("form")[1];
+const signinForm = document.getElementById("signin-form");
+const registerForm = document.getElementById("register-form");
 // Form inputs
 const formInputs = document.querySelectorAll("input");
-const infoBox = document.querySelector(".info-box");
+const infoBox1 = document.querySelectorAll(".info-box")[0];
+const infoBox2 = document.querySelectorAll(".info-box")[1];
 
-// User database
-let users = [];
+let allUsers = [];
 
 function togglePageShown(page) {
     if (page === "signin") {
@@ -28,65 +28,172 @@ function togglePageShown(page) {
     }
 }
 
-function getSigninInput() {
-    const emailValue = formInputs[0].value;
-    const passwordValue = formInputs[1].value;
-    let storedEmail = "";
-    let storedPassword = "";
-    // Loop through the users array and check if any object there matches the entered credentials
-    users.forEach(user => {
-        if (emailValue === user.email && passwordValue === user.password) {
-            storedEmail = user.email;
-            storedPassword = user.password;
+function getSigninValues(e) {
+    e.preventDefault();
+    // Get signin input values
+    const emailInput = formInputs[0].value;
+    const passwordInput = formInputs[1].value;
+    let userEmail = "";
+    let userPassword = "";
+    allUsers.forEach(user => {
+        if (user.email === emailInput && user.password === passwordInput) {
+            userEmail = user.email;
+            userPassword = user.password;
         }
     });
-    if (emailValue === storedEmail && passwordValue === storedPassword) {
-        console.log("logged in");
+    // If email and password in allUsers array matches the email and password input, then log in
+    if (userEmail === emailInput && userPassword === passwordInput) {
+        console.log("Logged in");
+        infoBox1.classList.add("hidden");
     } else {
-        console.log("not logged in");
+        console.log("NOT LOGGED IN");
+        infoBox1.textContent = "Incorrect email or password";
+        infoBox1.style.border = "2px solid red";
+        infoBox1.classList.remove("hidden");
     }
-
 }
 
-function getRegisterInput() {
-    const nameValue = formInputs[2].value;
-    const emailValue = formInputs[3].value;
-    const passwordValue = formInputs[4].value;
-    const passwordConfirm = formInputs[5].value;
+function getRegisterValues(e) {
+    e.preventDefault();
     const newUser = {};
-    if (passwordValue === passwordConfirm) {
-        // Append details to an object
-        newUser.name = nameValue;
-        newUser.email = emailValue;
-        newUser.password = passwordValue;
-        // Push the new user object to the users array
-        users.push(newUser);
-        updateLocalStorage();
-        // Display a green outline box notifying that the account has been created
-        infoBox.textContent = "Account created succesfully";
-        infoBox.style.border = "2px solid lightgreen";
-        infoBox.classList.remove("hidden");
+    // Get register input values
+    const nameInput = formInputs[2].value;
+    const emailInput = formInputs[3].value;
+    const passwordInput = formInputs[4].value;
+    const confirmInput = formInputs[5].value;
+    // If passwords entered match, then display the infoBox2 and append data to user object
+    if (passwordInput === confirmInput) {
+        infoBox2.textContent = "Account successfully created";
+        infoBox2.style.border = "2px solid lightgreen";
+        infoBox2.classList.remove("hidden");
+        // Append keys and values to the newUser object
+        newUser.name = nameInput;
+        newUser.email = emailInput;
+        newUser.password = passwordInput;
+        allUsers.push(newUser);
+        saveUsersToLocalStorage();
+        test(0);
     } else {
-        infoBox.classList.remove("hidden");
+        infoBox2.textContent = "Make sure both passwords match";
+        infoBox2.style.border = "2px solid red";
+        infoBox2.classList.remove("hidden");
+        test(4);
     }
 }
 
-// Save the users array to local storage
-function updateLocalStorage() {
-    localStorage.setItem("users", JSON.stringify(users));
+// Function that will run to clear the input fields after form submissions
+function clearInputFields() {
+    formInputs.forEach(input => {
+        input.value = "";
+    });
 }
 
-// Update users array from the local storage
+// Delete passwords values or all input values, depending on if the user enetered both passwords correctly
+function test(index) {
+    for (let i = index; i < formInputs.length; i++) {
+        console.log(formInputs[i]);
+        formInputs[i].value = "";
+    }
+}
+
+// Save allUsers array to local storage
+function saveUsersToLocalStorage() {
+    localStorage.setItem("users", JSON.stringify(allUsers));
+}
+
+// Update users array with data from local storage
 function updateUsersArray() {
-    users = JSON.parse(localStorage.getItem("users"))
+    if (localStorage.getItem("users")) {
+        allUsers = JSON.parse(localStorage.getItem("users"));
+        console.log(allUsers);
+    }
 }
 
 // Event listeners
 signinBtn.addEventListener("click", () => togglePageShown("signin"));
 registerBtn.addEventListener("click", () => togglePageShown("register"));
-signinForm.addEventListener("submit", getSigninInput);
-registerForm.addEventListener("submit", getRegisterInput);
+signinForm.addEventListener("submit", getSigninValues);
+registerForm.addEventListener("submit", getRegisterValues);
 
 // Run function on page load
 togglePageShown("signin");
 updateUsersArray();
+
+
+
+
+// // User database
+// let users = [];
+// const newUser = {};
+
+
+
+// function getSigninInput() {
+//     const emailValue = formInputs[0].value;
+//     const passwordValue = formInputs[1].value;
+//     let storedEmail = "";
+//     let storedPassword = "";
+//     // Loop through the users array and check if any object there matches the entered credentials
+//     users.forEach(user => {
+//         if (emailValue === user.email && passwordValue === user.password) {
+//             storedEmail = user.email;
+//             storedPassword = user.password;
+//         }
+//     });
+//     // If credentials entered match the credentials in users then sign in, else display error
+//     if (emailValue === storedEmail && passwordValue === storedPassword) {
+//         console.log("logged in");
+//         infoBox1.classList.add("hidden");
+//     } else {
+//         console.log("not logged in");
+//         infoBox1.classList.remove("hidden");
+//         infoBox1.textContent = "Incorrect email address or password";
+//     }
+//     // Remove password after attempted log in
+//     return formInputs[1] = "";
+// }
+
+// function getRegisterInput() {
+//     const nameValue = formInputs[2].value;
+//     const emailValue = formInputs[3].value;
+//     const passwordValue = formInputs[4].value;
+//     const passwordConfirm = formInputs[5].value;
+//     if (passwordValue === passwordConfirm) {
+//         // Append details to an object
+//         newUser.name = nameValue;
+//         newUser.email = emailValue;
+//         newUser.password = passwordValue;
+//         // Push the new user object to the users array
+//         // Display a green outline box notifying that the account has been created
+//         infoBox2.textContent = "Account created succesfully";
+//         infoBox2.style.border = "2px solid lightgreen";
+//         infoBox2.classList.remove("hidden");
+//     } else {
+//         infoBox2.textContent = "Please make sure both passwords match";
+//         infoBox2.classList.remove("hidden");
+//     }
+//     users.push(newUser);
+//     updateLocalStorage();
+// }
+
+// // Save the users array to local storage
+// function updateLocalStorage() {
+//     if (users.length) {
+//         localStorage.setItem("users", JSON.stringify(users));
+//     }
+// }
+
+// // Update users array from the local storage
+// function updateUsersArray() {
+//     users = JSON.parse(localStorage.getItem("users"));
+// }
+
+// Event listeners
+// signinBtn.addEventListener("click", () => togglePageShown("signin"));
+// registerBtn.addEventListener("click", () => togglePageShown("register"));
+// signinForm.addEventListener("submit", getSigninInput);
+// registerForm.addEventListener("submit", getRegisterInput);
+
+// // Run function on page load
+// togglePageShown("signin");
+// updateUsersArray();
